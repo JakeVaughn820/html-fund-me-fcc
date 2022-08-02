46409e8ff707c995a320c291a2c4591b5f77ccba
jakevaughn820 <jakevaughn820@gmail.com>
2022-08-02 14:25:43 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import { ethers } from "./ethers-5.6.esm.min.js"
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+connectButton.onclick = connect
+fundButton.onclick = fund
+
+async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+        try {
+            await ethereum.request({ method: "eth_requestAccounts" })
+        } catch (error) {
+            console.log(error)
+        }
+        connectButton.innerHTML = "Connected"
+        const accounts = await ethereum.request({ method: "eth_accounts" })
+        console.log(accounts)
+    } else {
+        connectButton.innerHTML = "Please install MetaMask"
+    }
+}
+
+async function fund() {
+    console.log(`Funding with ${ethAmount}...`)
+    if (typeof window.ethereum !== "undefined") {
+        try {
+            await ethereum.request({ method: "eth_requestAccounts" })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
